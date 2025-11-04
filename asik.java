ec3edb69221c54900fb17528e16b16994d25f59e
arzisukajs <github188@gmail.com>
2025-11-04 18:50:55 +0700

    Terima input pilihan mata kuliah dari pengguna

--- asik.java
+++ asik.java
@@ -92,6 +92,8 @@ public class asik {
             System.out.println("3. Basis Data");
             System.out.println("4. Algoritma dan Pemrograman");
             System.out.println("5. Rekayasa Perangkat Lunak");
+
+            int pilihan = ns.nextInt();
     }
 }
 }
